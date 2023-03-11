fcac2fa9b39e2ca81d4a5e0dc0d2994770f833d6
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-11 12:20:23 +0530

    tried hz scrlling effect

--- gsap-basics/script.js
+++ gsap-basics/script.js
@@ -5,7 +5,11 @@ gsap.to(".green",{
     fill:"blue",
     scrollTrigger:{
         trigger:".green",
-        toggleActions:"restart pause "
+        start:"top center",             
+        // Top means 'top of the selected div.
+        // center means '  center of the viewport height.
+        // markers:true,
+        toggleActions:"restart pause reverse none"
         // 1st value is for "when the bottom of the screen touches the given class"
         // 2nd value is for "when the top of screen touches the given class"
         // 3rd value is for "when the top of screen touches the given class"
@@ -14,4 +18,16 @@ gsap.to(".green",{
     rotation:360,
     duration:3,
 
+})
+
+gsap.to(".head",{
+    x:-600,
+    color:"white",
+    scrollTrigger:{
+        trigger:".head",
+        toggleActions:"play pause reverse none",
+        start:"top center",
+        markers:true,
+    },
+    duration:2,
 })
