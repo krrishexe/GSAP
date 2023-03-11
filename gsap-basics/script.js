7d3536605a720031a36b7fad77b10c5ac77f48ad
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-11 11:55:30 +0530

    added gsap basics

--- gsap-basics/script.js
+++ gsap-basics/script.js
@@ -1,5 +1,16 @@
-gsap.to("red",{
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".green",{
     x:400,
+    fill:"blue",
+    scrollTrigger:{
+        trigger:".green",
+        toggleActions:"restart pause "
+        // 1st value is for "when the bottom of the screen touches the given class"
+        // 2nd value is for "when the top of screen touches the given class"
+        // 3rd value is for "when the top of screen touches the given class"
+        
+    },
     rotation:360,
     duration:3,
 
