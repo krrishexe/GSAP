dda8c34eedb3c9e46f7d653713603c1e550f8b5c
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-11 12:32:56 +0530

    added scrub anims

--- gsap-basics/script.js
+++ gsap-basics/script.js
@@ -1,24 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".green",{
-    x:400,
-    fill:"blue",
-    scrollTrigger:{
-        trigger:".green",
-        start:"top center",             
-        // Top means 'top of the selected div.
-        // center means '  center of the viewport height.
-        // markers:true,
-        toggleActions:"restart pause reverse none"
-        // 1st value is for "when the bottom of the screen touches the given class"
-        // 2nd value is for "when the top of screen touches the given class"
-        // 3rd value is for "when the top of screen touches the given class"
+// gsap.to(".green",{
+//     x:400,
+//     fill:"blue",
+//     scrollTrigger:{
+//         trigger:".green",
+//         start:"top center",             
+//         // Top means 'top of the selected div.
+//         // center means '  center of the viewport height.
+//         // markers:true,
+//         toggleActions:"restart pause reverse none"
+//         // 1st value is for "when the bottom of the screen touches the given class"
+//         // 2nd value is for "when the top of screen touches the given class"
+//         // 3rd value is for "when the top of screen touches the given class"
         
-    },
-    rotation:360,
-    duration:3,
+//     },
+//     rotation:360,
+//     duration:3,
 
-})
+// })
 
 gsap.to(".head",{
     x:-600,
@@ -27,6 +27,7 @@ gsap.to(".head",{
         trigger:".head",
         toggleActions:"play pause reverse none",
         start:"top center",
+        scrub:true,
         markers:true,
     },
     duration:2,
